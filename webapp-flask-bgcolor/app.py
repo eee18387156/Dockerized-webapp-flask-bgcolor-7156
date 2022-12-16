3446acd521e4f1356f8481ca62927aee87c3c6cf
eee18387156 <120180638+eee18387156@users.noreply.github.com>
2022-12-16 15:19:37 +0200

    Update app.py

--- webapp-flask-bgcolor/app.py
+++ webapp-flask-bgcolor/app.py
@@ -54,9 +54,9 @@ if __name__ == "__main__":
     parser.add_argument('--title', required=False)
     args = parser.parse_args()
 
-    if args.color:
-        print("Color from command line argument =" + args.color)
-        COLOR = args.color
+    if args.colorhexcode:
+        print("Color from command line argument =" + args.colorhexcode)
+        COLOR = args.colorhexcode
         if COLOR_FROM_ENV:
             print("A color was set through environment variable -" + COLOR_FROM_ENV + ". However, color from command line argument takes precendence.")
     elif COLOR_FROM_ENV:
